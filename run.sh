71e63aabe4a5f318273494295277eb4583a9e925
blainehansen <faichenshing@gmail.com>
2018-04-08 14:42:00 -0600

    cleaned out go

--- run.sh
+++
@@ -1,4 +0,0 @@
-# sudo docker build -t go-anywhere .
-
-# sudo docker run --rm --name="crowd-sell" --net="host" -v `pwd`/server:/go/src/main -v `pwd`/server-bin:/go/bin -u `id -u`:`cut -d: -f3 < <(getent group $(whoami))` -it go-anywhere bash
-sudo docker run --rm --name="crowd-sell" --net="host" -v `pwd`/server:/go/src/main -v `pwd`/server-bin:/go/bin -it go-anywhere bash
