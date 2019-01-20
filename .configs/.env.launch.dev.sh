99f4ec81686e568bdc004ce89de18aa73837f5c4
Blaine Hansen <faichenshing@gmail.com>
2019-01-19 23:28:37 -0700

    getting there

---
+++ .configs/.env.launch.dev.sh
@@ -0,0 +1,11 @@
+ALLOW_ORIGIN='http://localhost:8080'
+
+SHOULD_MAIL='false'
+
+SERVER_PROTOCOL='http://'
+SERVER_DOMAIN='crowdsell.io'
+
+API_PORT='5050'
+API_URL='http://crowdsell:5050'
+
+DATABASE_SSL='disable'
---
+++ .configs/.env.launch.prod.sh
@@ -0,0 +1,11 @@
+ALLOW_ORIGIN='*'
+
+SHOULD_MAIL='true'
+
+SERVER_PROTOCOL='http://'
+SERVER_DOMAIN='crowdsell.io'
+
+API_PORT='5050'
+API_URL='http://api.crowdsell:5050'
+
+DATABASE_SSL='disable'
